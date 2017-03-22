9367f6e2e7953ea7f7f73bb915f12c2f01e69b9f
Guangyu Li <li@semcog.org>
2017-03-22 14:39:59 -0400

    move variables files to its own folder

---
+++ variables/__init__.py
@@ -0,0 +1,6 @@
+from variables_parcel import *
+from variables_access import *
+from variables_building import *
+from variables_demographic import *
+from variables_employment import *
+from variables_zone import *
