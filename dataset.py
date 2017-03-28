b06ab5fba8537fa78cbf37d69c70438092c2695e
Finkelman <finkelman@semcog.org>
2017-03-28 11:13:37 -0400

    pycharm reformat and clean dataset

--- dataset.py
+++ dataset.py
@@ -1,198 +1,173 @@
-import numpy as np
-import pandas as pd
-import os
-import assumptions
-from urbansim.utils import misc
-
 import warnings
 
-##
+import numpy as np
 import orca
-import utils
-
+import pandas as pd
 
 warnings.filterwarnings('ignore', category=pd.io.pytables.PerformanceWarning)
 
-@orca.table()
-def scheduled_development_events(store):
-    df = pd.read_csv("data/scheduled_development_events.csv")
-    return df
 
-@orca.table('jobs')
-def jobs(store):
-    df = store['jobs']
-    return df
+@orca.table()
+def scheduled_development_events():
+    return pd.read_csv("data/scheduled_development_events.csv")
+
 
 @orca.table()
 def buildings(store):
-    buildings = store['buildings']
-    buildings['sqft_price_nonres'] = buildings.improvement_value*1.0 / 0.7 / buildings.non_residential_sqft
-    buildings.sqft_price_nonres[buildings.sqft_price_nonres==np.inf] = 0
-    buildings.loc[buildings.sqft_price_nonres==np.inf,'sqft_price_nonres'] = 0
-    buildings['sqft_price_res'] = buildings.improvement_value*1.25 / 0.7 / (buildings.sqft_per_unit * buildings.residential_units)
-    buildings.loc[buildings.sqft_price_res == np.inf, 'sqft_price_res'] = 0
-    buildings.fillna(0, inplace=True)
-    return buildings
+    df = store['buildings']
+    df['sqft_price_nonres'] = df.improvement_value * 1.0 / 0.7 / df.non_residential_sqft
+    df.sqft_price_nonres[df.sqft_price_nonres == np.inf] = 0
+    df.loc[df.sqft_price_nonres == np.inf, 'sqft_price_nonres'] = 0
+    df['sqft_price_res'] = df.improvement_value * 1.25 / 0.7 / (df.sqft_per_unit * df.residential_units)
+    df.loc[df.sqft_price_res == np.inf, 'sqft_price_res'] = 0
+    df.fillna(0, inplace=True)
+    return df
+
 
 @orca.table()
 def households(store):
     df = store['households']
     df.loc[df.building_id == -1, 'building_id'] = np.random.choice(store.buildings.index.values,
                                                                    (df.building_id == -1).sum())
-    idx_invalid_building_id = np.in1d(df.building_id, store.buildings.index.values) == False
+    idx_invalid_building_id = np.in1d(df.building_id, store.buildings.index.values) is False
     df.loc[idx_invalid_building_id, 'building_id'] = np.random.choice(store.buildings.index.values,
                                                                       idx_invalid_building_id.sum())
     return df
-    
+
+
+@orca.table()
+def jobs(store):
+    return store['jobs']
+
+
 @orca.table()
 def persons(store):
-    df = store['persons']
-    return df
+    return store['persons']
+
 
 @orca.table()
 def parcels(store):
-    df = store['parcels']
-    return df
+    return store['parcels']
+
 
 @orca.table()
 def zones(store):
-    df = store['zones']
-    return df
-    
+    return store['zones']
+
+
 @orca.table()
 def cities(store):
-    df = store['cities']
-    return df
+    return store['cities']
 
 
 @orca.table()
 def counties(store):
-    df = store['counties']
-    return df
+    return store['counties']
 
 
 @orca.table()
 def employment_sectors(store):
-    df = store['employment_sectors']
-    return df
+    return store['employment_sectors']
 
 
 @orca.table()
 def home_based_status(store):
-    df = store['home_based_status']
-    return df
+    return store['home_based_status']
 
 
 @orca.table()
 def target_vacancies(store):
-    df = store['target_vacancies']
-    return df
+    return store['target_vacancies']
 
 
 @orca.table()
 def building_sqft_per_job(store):
-    df = store['building_sqft_per_job']
-    return df
-    
+    return store['building_sqft_per_job']
+
+
 @orca.table()
 def annual_relocation_rates_for_households(store):
-    df = store['annual_relocation_rates_for_households']
-    return df
-    
+    return store['annual_relocation_rates_for_households']
+
+
 @orca.table()
 def annual_relocation_rates_for_jobs(store):
-    df = store['annual_relocation_rates_for_jobs']
-    return df
-    
+    return store['annual_relocation_rates_for_jobs']
+
+
 @orca.table()
 def annual_household_control_totals(store):
-    df = store['annual_household_control_totals']
-    return df
+    return store['annual_household_control_totals']
+
 
 @orca.table()
 def annual_employment_control_totals(store):
-    df = store['annual_employment_control_totals']
-    return df
-    
+    return store['annual_employment_control_totals']
+
+
 @orca.table()
 def travel_data(store):
-    df = store['travel_data']
-    return df
+    return store['travel_data']
 
 
 @orca.table()
 def zoning(store):
-    df = store['zoning']
-    return df
+    return store['zoning']
+
 
 @orca.table()
 def large_areas(store):
-    df = store['large_areas']
-    return df
+    return store['large_areas']
+
 
 @orca.table()
 def building_types(store):
-    df = store['building_types']
-    return df
+    return store['building_types']
+
 
 @orca.table()
 def land_use_types(store):
-    df = store['land_use_types']
-    return df
+    return store['land_use_types']
 
 
 @orca.table()
 def access_drive_minutes(store):
-    df = store['access_drive_minutes']
-    return df
+    return store['access_drive_minutes']
 
 
 @orca.table()
 def access_walk_feet(store):
-    df = store['access_walk_feet']
-    return df
+    return store['access_walk_feet']
 
 
 @orca.table()
 def transit_stops(store):
-    df = store['transit_stops']
-    return df
-
-
-# these are dummy returns that last until accessibility runs
-@orca.table()
-def nodes():
-    return pd.DataFrame()
-
-
-@orca.table()
-def nodes_walk():
-    return pd.DataFrame()
-
-
-@orca.table()
-def nodes_drv():
-    return pd.DataFrame()
+    return store['transit_stops']
 
 
 @orca.table()
 def crime_rates(store):
-    df = store['crime_rates']
-    return df
+    return store['crime_rates']
 
 
 @orca.table()
 def schools(store):
-    df = store['schools']   
-    return df
+    return store['schools']
+
 
 @orca.table()
 def poi(store):
-    df = store['poi']   
-    return df
+    return store['poi']
+
+
+# these are dummy returns that last until accessibility runs
+for gq_tbl in ['nodes', 'nodes_walk', 'nodes_drv']:
+    empty_df = pd.DataFrame()
+    orca.add_table(gq_tbl, empty_df)
 
 # GQ placeholders
-for gq_tbl in ['tazcounts2040gq', 'tazcounts2015gq', 'tazcounts2020gq', 'tazcounts2035gq', 'tazcounts2025gq', 'tazcounts2030gq']:
+for gq_tbl in ['tazcounts2040gq', 'tazcounts2015gq', 'tazcounts2020gq', 'tazcounts2035gq', 'tazcounts2025gq',
+               'tazcounts2030gq']:
     empty_df = pd.DataFrame()
     orca.add_table(gq_tbl, empty_df)
 
@@ -210,5 +185,3 @@ orca.broadcast('households', 'persons', cast_index=True, onto_on='household_id')
 orca.broadcast('building_types', 'buildings', cast_index=True, onto_on='building_type_id')
 orca.broadcast('zones', 'parcels', cast_index=True, onto_on='zone_id')
 orca.broadcast('schools', 'parcels', cast_on='parcel_id', onto_index=True)
-
-
