90bf71ac15f186c995825898fe8953ffb8824981
Guangyu Li <li@semcog.org>
2017-06-21 17:58:36 -0400

    default dose not work for out_base_tables

--- Simulation.py
+++ Simulation.py
@@ -4,19 +4,6 @@ from urbansim.utils import misc, networks
 
 orca.run(['build_networks'])
 
-base_tables = ['jobs', 'employment_sectors', 'annual_relocation_rates_for_jobs',
-               'households', 'persons', 'annual_relocation_rates_for_households',
-               'buildings', 'parcels', 'zones', 'semmcds', 'counties',
-               'target_vacancies', 'building_sqft_per_job',
-               'annual_employment_control_totals',
-               'travel_data', 'zoning', 'large_areas', 'building_types', 'land_use_types',
-               'workers_labor_participation_rates', 'workers_employment_rates_by_large_area_age',
-               'workers_employment_rates_by_large_area',
-               'transit_stops', 'crime_rates', 'schools', 'poi',
-               'annual_household_control_totals', 'scheduled_development_events', 'scheduled_demolition_events']
-
-out_tables = ['buildings', 'jobs', 'parcels', 'households', 'persons', 'dropped_buildings']
-
 orca.run([
     "neighborhood_vars",  # worked
     "households_transition",  # worked
@@ -41,7 +28,16 @@ orca.run([
     # "housing_value_update", Fixme: maybe we don't need
 ], iter_vars=range(2016, 2045 + 1),
     data_out=utils.get_run_filename(),
-    out_base_tables=base_tables, #TODO: check pass None to get all tables for base  year
-    out_run_tables=out_tables,
+    out_base_tables=['jobs', 'employment_sectors', 'annual_relocation_rates_for_jobs',
+                     'households', 'persons', 'annual_relocation_rates_for_households',
+                     'buildings', 'parcels', 'zones', 'semmcds', 'counties',
+                     'target_vacancies', 'building_sqft_per_job',
+                     'annual_employment_control_totals',
+                     'travel_data', 'zoning', 'large_areas', 'building_types', 'land_use_types',
+                     'workers_labor_participation_rates', 'workers_employment_rates_by_large_area_age',
+                     'workers_employment_rates_by_large_area',
+                     'transit_stops', 'crime_rates', 'schools', 'poi',
+                     'annual_household_control_totals', 'scheduled_development_events', 'scheduled_demolition_events'],
+    out_run_tables=['buildings', 'jobs', 'parcels', 'households', 'persons', 'dropped_buildings'],
     out_interval=1)
 # TODO: check target_vacancies used or not and how
