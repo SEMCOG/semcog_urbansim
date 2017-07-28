b4f6016f24b783ae86dac7d157ac3890261fd4d8
Guangyu Li <li@semcog.org>
2017-07-28 16:49:20 -0400

    Try new data with some imputed max_dua data

--- assumptions.py
+++ assumptions.py
@@ -89,7 +89,7 @@ orca.add_injectable("form_to_btype", {
 
 
 def verify():
-    hdf_store = pd.HDFStore(os.path.join(misc.data_dir(), "all_semcog_data_07-18-17.h5"), mode="r")
+    hdf_store = pd.HDFStore(os.path.join(misc.data_dir(), "all_semcog_data_07-28-17.h5"), mode="r")
 
     new = verify_data_structure.yaml_from_store(hdf_store)
     with open(r"configs/data_structure.yaml", "w") as out:
