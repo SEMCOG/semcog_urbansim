17ec6a116cbffad6f5d16d8d7e0c8fbeaeeae638
Guangyu Li <li@semcog.org>
2018-01-23 15:57:17 -0500

    reorder job events

--- Simulation.py
+++ Simulation.py
@@ -18,14 +18,15 @@ orca.run(["refiner",
 
 orca.run([
     "neighborhood_vars",
+    "scheduled_demolition_events",
+    "random_demolition_events",
+    "scheduled_development_events",
+    "refiner",       
     "households_transition",
     "fix_lpr",
     "households_relocation",
     "jobs_transition",
     "jobs_relocation",
-    "scheduled_demolition_events",
-    "random_demolition_events",
-    "scheduled_development_events",
     "feasibility",
     "residential_developer",
     "non_residential_developer"] +
@@ -36,7 +37,6 @@ orca.run([
     ["elcm_home_based",
     "government_jobs_scaling_model",
     "gq_pop_scaling_model",
-    "refiner",
     # "travel_model", Fixme: on hold
 ],
     iter_vars=range(2016, 2045 + 1),
