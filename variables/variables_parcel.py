c32a7cca7d56869c1db3674cf2b5b8c0f3f78180
Finkelman <finkelman@semcog.org>
2018-01-26 07:59:52 -0500

    oopps

--- variables/variables_parcel.py
+++ variables/variables_parcel.py
@@ -58,7 +58,7 @@ def parcel_is_allowed(form=None):
         hu_target = orca.get_table('extreme_hu_controls').to_frame()
         hu_target['c0'] = (hu_target.end - hu_target.base) * (year - 2015) + hu_target.base
         hu_target['c5'] = (hu_target.end - hu_target.base) * (year - 2015 + 5) + hu_target.base
-        city_is_full = count_hu > hu_target[['c0', 'c5']].max(1)
+        city_is_full = count_hu > hu_target[['c0', 'c5']].max(1).reindex(count_hu.index, fill_value=0)
         city_is_full = city_is_full[city_is_full]
         p_city = orca.get_table('parcels').city_id
         city_is_full = p_city.isin(city_is_full.index).reindex(index, fill_value=False)
