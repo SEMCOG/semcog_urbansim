0d345c4a8d1bbcadbf323e0184b26aec21fd2480
semcogli <li@semcog.org>
2022-09-01 11:42:03 -0400

    add intercept to lasso estimation

--- REPM_feature_selection.py
+++ REPM_feature_selection.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+
 # coding: utf-8
 # feature selection
 # 1. MRMR
 # 2. ANOVA F-test - applicable for both numeric and categorical
 # 3. use old estimation result as guide
-# Performance optimization 
+# Performance optimization
 # - sample buildings by type and then fit
-# - sample vars 
+# - sample vars
 from urbansim.models.regression import RegressionModel
 
 import os
@@ -18,13 +19,17 @@ import sys
 import scipy
 import time
 from tqdm import tqdm
+
 # from guppy import hpy; h=hpy()
 # import pymrmr
 
 # suppress sklearn warnings
 def warn(*args, **kwargs):
     pass
+
+
 import warnings
+
 warnings.warn = warn
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.preprocessing import StandardScaler
@@ -38,8 +43,8 @@ from data_checks.estimation_variables_2050 import *
 
 
 # import utils
-# data_path = r"/home/da/share/U_RDF2050/model_inputs/base_hdf"
-data_path = r'/home/da/share/urbansim/RDF2050/model_inputs/base_hdf'
+data_path = r"/home/da/share/U_RDF2050/model_inputs/base_hdf"
+# data_path = r'/home/da/share/urbansim/RDF2050/model_inputs/base_hdf'
 hdf_list = [
     (data_path + "/" + f)
     for f in os.listdir(data_path)
@@ -55,9 +60,9 @@ var_validation_list = [
     if ("variable_validation" in f) & (f[-5:] == ".yaml")
 ]
 var_validation_last = max(var_validation_list, key=os.path.getctime)
-with open(var_validation_last, 'r') as f:
+with open(var_validation_last, "r") as f:
     vars_config = yaml.load(f, Loader=yaml.FullLoader)
-valid_b_vars = vars_config['buildings']['valid variables']
+valid_b_vars = vars_config["buildings"]["valid variables"]
 
 
 orca.add_injectable("store", hdf)
@@ -69,20 +74,60 @@ orca.run(["build_networks"])
 orca.run(["neighborhood_vars"])
 
 vars_to_skip = [
-    'large_area_id', 'county_id',
-    'parcel_id', 'st_parcel_id', 'geoid', 'st_geoid', 'b_ln_parcels_parcel_far', 'parcels_parcel_far',
-    'parcels_st_parcel_far', 'parcels_census_bg_id',
-    'nodeid_drv', 'st_nodeid_drv', 'nodeid_walk', 'st_nodeid_walk', 'semmcd', 
-    'city_id', 'census_bg_id', 'x', 'y',
-    'zone_id', 'improvement_value', 'market_value', 'landvalue', 'parcels_landvalue', 'b_ln_sqft_price_nonres',
-    'zones_tazce10_n', 'tazce10', 'parcels_zones_tazce10_n', 'parcels_st_zones_tazce10_n',
-    'st_parcels_zones_tazce10_n', 'st_parcels_st_zones_tazce10_n', 'st_zones_tazce10_n',
-    'parcels_land_use_type_id', 'st_parcels_land_use_type_id', 'st_parcels_max_dua', 'parcels_max_dua',
-    'parcels_max_height', 'st_parcels_max_height', 'parcels_school_id', 'st_parcels_school_id', 
-    'parcels_sev_value', 'st_parcels_sev_value', 'parcels_centroid_x', 'st_parcels_centroid_x',
-    'parcels_centroid_y', 'st_parcels_centroid_y', 'parcels_school_id', 'st_parcels_school_id',
-    'parcels_land_cost', 'st_parcels_land_cost', 'b_ln_market_value', 'st_b_ln_market_value'
+    "large_area_id",
+    "county_id",
+    "parcel_id",
+    "st_parcel_id",
+    "geoid",
+    "st_geoid",
+    "b_ln_parcels_parcel_far",
+    "parcels_parcel_far",
+    "parcels_st_parcel_far",
+    "parcels_census_bg_id",
+    "nodeid_drv",
+    "st_nodeid_drv",
+    "nodeid_walk",
+    "st_nodeid_walk",
+    "semmcd",
+    "city_id",
+    "census_bg_id",
+    "x",
+    "y",
+    "zone_id",
+    "improvement_value",
+    "market_value",
+    "landvalue",
+    "parcels_landvalue",
+    "b_ln_sqft_price_nonres",
+    "zones_tazce10_n",
+    "tazce10",
+    "parcels_zones_tazce10_n",
+    "parcels_st_zones_tazce10_n",
+    "st_parcels_zones_tazce10_n",
+    "st_parcels_st_zones_tazce10_n",
+    "st_zones_tazce10_n",
+    "parcels_land_use_type_id",
+    "st_parcels_land_use_type_id",
+    "st_parcels_max_dua",
+    "parcels_max_dua",
+    "parcels_max_height",
+    "st_parcels_max_height",
+    "parcels_school_id",
+    "st_parcels_school_id",
+    "parcels_sev_value",
+    "st_parcels_sev_value",
+    "parcels_centroid_x",
+    "st_parcels_centroid_x",
+    "parcels_centroid_y",
+    "st_parcels_centroid_y",
+    "parcels_school_id",
+    "st_parcels_school_id",
+    "parcels_land_cost",
+    "st_parcels_land_cost",
+    "b_ln_market_value",
+    "st_b_ln_market_value",
 ]
+
 
 def estimate_repm(yaml_config):
     model = RegressionModel.from_yaml(str_or_buffer=misc.config(yaml_config))
@@ -91,32 +136,41 @@ def estimate_repm(yaml_config):
     model.to_yaml(str_or_buffer=misc.config(yaml_config))
     return model.fit_parameters
 
+
 def apply_filter_query(df, filters=None):
     if filters:
         if isinstance(filters, str):
             query = filters
         else:
-            query = ' and '.join(filters)
+            query = " and ".join(filters)
         return df.query(query)
     else:
         return df
 
+
 def generate_repm_config(mat, vars_used):
-    filter_cols = ['sqft_price_nonres', 'sqft_price_res', 'non_residential_sqft',  'hedonic_id', 'residential_units']
+    filter_cols = [
+        "sqft_price_nonres",
+        "sqft_price_res",
+        "non_residential_sqft",
+        "hedonic_id",
+        "residential_units",
+    ]
     filter_col_ind = [vars_used.index(name) for name in filter_cols]
-    df = pd.DataFrame(np.vstack([mat.getrow(i).todense() for i in filter_col_ind]).transpose(), columns=filter_cols)
-    df['hedonic_id'] = df['hedonic_id'].astype('int')
+    df = pd.DataFrame(
+        np.vstack([mat.getrow(i).todense() for i in filter_col_ind]).transpose(),
+        columns=filter_cols,
+    )
+    df["hedonic_id"] = df["hedonic_id"].astype("int")
     hid_count = df.hedonic_id.value_counts()
     for hid in hid_count.index:
         config = {
-            'name': 'RegressionModel',
-            'model_type': 'Regression',
-            'fit_filters': [],
-            'predict_filters': 'hedonic_id == ' + str(hid),
-            'ytransform': 'np.exp',
-            'model_expression': {
-                'left_side': ''
-            }
+            "name": "RegressionModel",
+            "model_type": "Regression",
+            "fit_filters": [],
+            "predict_filters": "hedonic_id == " + str(hid),
+            "ytransform": "np.exp",
+            "model_expression": {"left_side": ""},
         }
         if hid % 100 in [81, 82, 83, 84]:
             prefix = "res_repm"
@@ -126,43 +180,58 @@ def generate_repm_config(mat, vars_used):
             prefix = "nonres_repm"
             price_col = "sqft_price_nonres"
             size_col = "non_residential_sqft"
-        config['fit_filters'].append(size_col + ' > 0')
-        config['fit_filters'].append(price_col + ' > 1')
-        config['fit_filters'].append(price_col + ' < 650')
-        config['fit_filters'].append('hedonic_id == ' + str(hid))
-        config['model_expression']['left_side'] = 'np.log1p(%s)' % price_col
+        config["fit_filters"].append(size_col + " > 0")
+        config["fit_filters"].append(price_col + " > 1")
+        config["fit_filters"].append(price_col + " < 650")
+        config["fit_filters"].append("hedonic_id == " + str(hid))
+        config["model_expression"]["left_side"] = "np.log1p(%s)" % price_col
         config_name = prefix + str(hid)
-        with open('configs/repm_2050/%s.yaml' % config_name, 'w') as f:
+        with open("configs/repm_2050/%s.yaml" % config_name, "w") as f:
             yaml.dump(config, f, default_flow_style=False)
     return
 
+
 def get_training_data(mat, yaml_config, vars_used):
     # yaml with less than 100 samples
-    # 42, 52, 91, 92, 93, 95, 
-    # 311, 313, 314, 341, 371, 
-    # 511, 514, 561, 563, 571, 
+    # 42, 52, 91, 92, 93, 95,
+    # 311, 313, 314, 341, 371,
+    # 511, 514, 561, 563, 571,
     # 9311, 9313, 9314, 9333, 9341, 9351, 9361, 9363, 9371, 9383,
     # 9911, 9913, 9914, 9941, 9961, 9963, 9971, 9984,
     # 11511, 11513, 11514, 11533, 11541, 11541, 11561, 11563, 11571, 11584,
     # 12511, 12513, 12514, 12571, 12584,
     # 14711, 14713, 14714, 14733, 14741, 14761, 14763, 14771, 14784,
-    # 16111, 16113, 16114, 16141, 16161, 16163, 16171, 16184, 
-    filter_cols = ['sqft_price_nonres', 'sqft_price_res', 'non_residential_sqft',  'hedonic_id', 'residential_units']
+    # 16111, 16113, 16114, 16141, 16161, 16163, 16171, 16184,
+    filter_cols = [
+        "sqft_price_nonres",
+        "sqft_price_res",
+        "non_residential_sqft",
+        "hedonic_id",
+        "residential_units",
+    ]
     filter_col_ind = [vars_used.index(name) for name in filter_cols]
-    df = pd.DataFrame(np.vstack([mat.getrow(i).todense() for i in filter_col_ind]).transpose(), columns=filter_cols)
-    with open( yaml_config, 'r') as f:
+    df = pd.DataFrame(
+        np.vstack([mat.getrow(i).todense() for i in filter_col_ind]).transpose(),
+        columns=filter_cols,
+    )
+    with open(yaml_config, "r") as f:
         conf = yaml.load(f, Loader=yaml.FullLoader)
-    fit_filters = conf['fit_filters']
-    target_var = conf['model_expression']['left_side']
-    target_var_raw = target_var[target_var.find("(")+1:target_var.find(")")]
+    fit_filters = conf["fit_filters"]
+    target_var = conf["model_expression"]["left_side"]
+    target_var_raw = target_var[target_var.find("(") + 1 : target_var.find(")")]
     filtered_df = apply_filter_query(df, fit_filters)
-    if filtered_df.size == 0: 
+    if filtered_df.size == 0:
         print("Config %s has 0 size dataframe after filtering" % (yaml_config))
-        return [0 for _ in range(len(vars_used))], [0 for _ in range(len(vars_used))], None, filtered_df.size
+        return (
+            [0 for _ in range(len(vars_used))],
+            [0 for _ in range(len(vars_used))],
+            None,
+            filtered_df.size,
+        )
     else:
         print("Config %s has sample size: %s" % (yaml_config, filtered_df.size))
     filtered_ind = filtered_df.index
-    # get filtered rows, if load all variables, sys will stalled 
+    # get filtered rows, if load all variables, sys will stalled
     # all_vars_filtered = pd.DataFrame(np.hstack([mat.getcol(i).todense() for i in filtered_ind]).transpose(), columns=vars_used)
     cols_ind_to_fit = [i for i in range(mat.shape[0]) if i not in filter_col_ind]
     cols_names_to_fit = [vars_used[i] for i in cols_ind_to_fit]
@@ -171,40 +240,64 @@ def get_training_data(mat, yaml_config, vars_used):
     y = filtered_df.loc[:, target_var_raw].values
     # target transformation
     y = np.log1p(y)
-    return np.nan_to_num(all_vars_filtered, copy=False), y, cols_names_to_fit, all_vars_filtered.shape[0]
+    return (
+        np.nan_to_num(all_vars_filtered, copy=False),
+        y,
+        cols_names_to_fit,
+        all_vars_filtered.shape[0],
+    )
+
 
 def feature_selection_f_classif(mat, yaml_config, vars_used):
-    all_vars_filtered, y, cols_names_to_fit, sample_size = get_training_data(mat, yaml_config, vars_used)
+    all_vars_filtered, y, cols_names_to_fit, sample_size = get_training_data(
+        mat, yaml_config, vars_used
+    )
     f, p = f_classif(all_vars_filtered, y)
     return f, p
 
+
 def feature_selection_lasso(mat, yaml_config, vars_used):
-    all_vars_filtered, y, cols_names_to_fit, sample_size = get_training_data(mat, yaml_config, vars_used)
+    all_vars_filtered, y, cols_names_to_fit, sample_size = get_training_data(
+        mat, yaml_config, vars_used
+    )
     if sample_size == 0:
         return None, None, None, sample_size
-    pipeline = Pipeline([
-                        # ('scaler',StandardScaler()), # disable standardize
-                        # alpha control the strength of regularization, 
-                        # higher the alpha, stronger the regularization, more vars get 0 coef
-                        ('model',Lasso(alpha=0.3))
-    ])
+    pipeline = Pipeline(
+        [
+            # ('scaler',StandardScaler()), # disable standardize
+            # alpha control the strength of regularization,
+            # higher the alpha, stronger the regularization, more vars get 0 coef
+            ("model", Lasso(alpha=0.3, fit_intercept=True))
+        ]
+    )
     # search = GridSearchCV(pipeline,
     #                   {'model__alpha':np.arange(0.1,10,0.1)},
     #                   cv = 5, scoring="neg_mean_squared_error",verbose=3
     #                 )
     pipeline.fit(all_vars_filtered, y)
-    cols_names = [cols_names_to_fit[i] for i in np.arange(all_vars_filtered.shape[1])[np.abs(pipeline._final_estimator.coef_) > 0]]
-    coef = pipeline._final_estimator.coef_[np.abs(pipeline._final_estimator.coef_) > 0]
+    cols_names = [
+        cols_names_to_fit[i]
+        for i in np.arange(all_vars_filtered.shape[1])[
+            np.abs(pipeline._final_estimator.coef_) > 0
+        ]
+    ]
+    coef = list(
+        pipeline._final_estimator.coef_[np.abs(pipeline._final_estimator.coef_) > 0]
+    )
+    intercept = pipeline._final_estimator.intercept_
     score = pipeline.score(all_vars_filtered, y)
+    cols_names.insert(0, "Intercept")
+    coef.insert(0, intercept)
+
     print(score)
     print(cols_names)
     print(coef)
+    print(intercept)
     # f, p = f_classif(np.nan_to_num(all_vars_filtered, copy=False), y)
     # return f, p
     # print(search.best_params_)
     # {'model__alpha': 0.30000000000000004}
     return cols_names, coef, score, sample_size
-
 
 
 def load_variables():
@@ -215,7 +308,7 @@ def load_variables():
     mat_list = []
     for i, var in enumerate(tqdm(valid_b_vars[:])):
         # skip variables in the list or if it's standardized variable
-        if var in vars_to_skip or 'st_' in var or var not in buildings.columns:
+        if var in vars_to_skip or "st_" in var or var not in buildings.columns:
             continue
         s = buildings.to_frame(var).iloc[:, 0]
         if pd.api.types.is_numeric_dtype(s):
@@ -223,7 +316,8 @@ def load_variables():
             sparse_mat = scipy.sparse.csr_matrix(s.values)
             mat_list.append(sparse_mat)
         # clear cache every 50 vars
-        if i % 50 ==0: orca.clear_columns('buildings')
+        if i % 50 == 0:
+            orca.clear_columns("buildings")
     # clear all orca cache
     orca.clear_all()
     mat = scipy.sparse.vstack(mat_list)
@@ -232,13 +326,14 @@ def load_variables():
     print("finsihed in ", t1 - t0)
     return vars_used, mat
 
-buildings = orca.get_table('buildings')
+
+buildings = orca.get_table("buildings")
 # load variables
 n = len(valid_b_vars)
 vars_used, mat = load_variables()
 # cannot use orca from now on
 
-# remove old REPM configs 
+# remove old REPM configs
 path = "./configs/repm_2050/"
 for f in os.listdir(path):
     os.remove(os.path.join(path, f))
@@ -268,30 +363,29 @@ repm_configs.sort()
 
 result = {}
 for config in repm_configs:
-	col_names, coef, score, sample_size = feature_selection_lasso(mat, config, vars_used)
-	if sample_size == 0:
-		result[config] = {
-            'coef': {},
-            'score': 0.0,
-            'sample_size': sample_size
+    col_names, coef, score, sample_size = feature_selection_lasso(
+        mat, config, vars_used
+    )
+    if sample_size == 0:
+        result[config] = {"coef": {}, "score": 0.0, "sample_size": sample_size}
+    else:
+        result[config] = {
+            "coef": {col_names[i]: float(coef[i]) for i in range(len(col_names))},
+            "score": score,
+            "sample_size": sample_size,
         }
-	else:
-		result[config] = {
-			'coef': {col_names[i]: float(coef[i]) for i in range(len(col_names))},
-			'score': score,
-			'sample_size': sample_size
-		}
-	if type(result[config]['sample_size']) == np.int64: 
-		result[config]['sample_size'] = result[config]['sample_size'].item()
-	# if result[config]['coef'] == 'None': continue
-	result[config]['score'] = float(result[config]['score'])
-	for coe in result[config]['coef']:
-		result[config]['coef'][coe] = float(result[config]['coef'][coe])
-	with open(config, 'a') as f:
-		yaml.dump(result[config], f, default_flow_style=False)
-    # f, p = feature_selection_f_classif(mat, config, vars_used)
-    # f = np.nan_to_num(f)
-    # p = np.nan_to_num(p)
-    # print('\t'.join([str(vars_used[x]) for x in np.argsort(p)[::-1][:10]]))
-    # print('\t'.join([str(p[x]) for x in np.argsort(p)[::-1][:10]]))
-print('done')
+    if type(result[config]["sample_size"]) == np.int64:
+        result[config]["sample_size"] = result[config]["sample_size"].item()
+    # if result[config]['coef'] == 'None': continue
+    result[config]["score"] = float(result[config]["score"])
+    for coe in result[config]["coef"]:
+        result[config]["coef"][coe] = float(result[config]["coef"][coe])
+    with open(config, "a") as f:
+        yaml.dump(result[config], f, default_flow_style=False)
+        # f, p = feature_selection_f_classif(mat, config, vars_used)
+        # f = np.nan_to_num(f)
+        # p = np.nan_to_num(p)
+        # print('\t'.join([str(vars_used[x]) for x in np.argsort(p)[::-1][:10]]))
+        # print('\t'.join([str(p[x]) for x in np.argsort(p)[::-1][:10]]))
+print("done")
+
