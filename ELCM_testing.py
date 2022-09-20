f908d3afdec5429da2fd9728a2eb48fb681170bd
Tian Xie <xie@semcog.org>
2022-09-20 10:20:53 -0400

    Ran HLCM with most used variables from last forecast; sample new mover HH; export theta

--- ELCM_testing.py
+++ ELCM_testing.py
@@ -10,6 +10,7 @@ from urbansim.utils import misc
 import sys
 import time
 from tqdm import tqdm
+import time
 
 from dcm_ard_libs import minimize, neglog_DCM
 
@@ -34,8 +35,8 @@ hdf_list = [
     if ("forecast_data_input" in f) & (f[-3:] == ".h5")
 ]
 hdf_last = max(hdf_list, key=os.path.getctime)
-# hdf = pd.HDFStore(hdf_last, "r")
-hdf = pd.HDFStore(data_path + "/" +"forecast_data_input_091422.h5", "r")
+hdf = pd.HDFStore(hdf_last, "r")
+# hdf = pd.HDFStore(data_path + "/" +"forecast_data_input_091422.h5", "r")
 print("HDF data: ", hdf_last)
 
 var_validation_list = [
@@ -126,18 +127,59 @@ def apply_filter_query(df, filters=None):
         return df
 
 
-def load_hlcm_df():
+def load_hlcm_df(hh_var, b_var):
     # load both hh
-    hh = households.to_frame([var for var in valid_hh_vars if var not in hh_vars_to_skip])
-    b = buildings.to_frame([var for var in valid_b_vars[:150] if var not in vars_to_skip] + ["large_area_id"])
+    hh = households.to_frame(hh_var)
+    b = buildings.to_frame(b_var)
     return hh, b
+
+def columns_in_vars(vars):
+    hh_columns, b_columns = [], []
+    for varname in vars:
+        if ':' in varname:
+            vs = varname.split(':')
+            hh_columns.append(vs[0])
+            b_columns.append(vs[1])
+        else:
+            if varname in valid_hh_vars:
+                hh_columns.append(varname)
+            elif varname in valid_b_vars:
+                b_columns.append(varname)
+            else:
+                print(varname, " not found in both hh and buildings table")
+    return hh_columns, b_columns
+            
+
+def get_interaction_vars( df, varname):
+    """Get interaction variables from variable name
+
+    Args:
+        varname (string): name of the interaction variable
+    """
+    if ":" in varname:
+        var1, var2 = varname.split(":")
+        return (df[var1] * df[var2]).values.reshape(-1, 1)
+    else:
+        return df[varname].values.reshape(-1, 1)
+
+@orca.column('households', cache=True, cache_scope='iteration')
+def residential_units(households, buildings):
+    return misc.reindex(buildings.residential_units, households.building_id)
+
+@orca.column('households', cache=True, cache_scope='iteration')
+def year_built(households, buildings):
+    return misc.reindex(buildings.year_built, households.building_id)
+
+@orca.column('households', cache=True, cache_scope='iteration')
+def mcd_model_quota(households, buildings):
+    return misc.reindex(buildings.mcd_model_quota, households.building_id)
 
 # config
 choice_column = "building_id"
-hh_sample_size = 100
-estimation_sample_size = 10
+hh_sample_size = 10000
+estimation_sample_size = 25
 # load variables
-RELOAD = False
+RELOAD = True
 if RELOAD:
     orca.add_injectable("store", hdf)
     load_tables_to_store()
@@ -147,37 +189,67 @@ if RELOAD:
     households = orca.get_table("households")
     orca.run(["build_networks"])
     orca.run(["neighborhood_vars"])
-    hh, b = load_hlcm_df()
+    orca.run(["mcd_hu_sampling"])
+    # TODO: get vars from vars list from last forecast
+    used_vars = pd.read_csv("/home/da/share/urbansim/RDF2050/model_improvements/2022_spring/HLCM_estimation/configs_hlcm_large_area_income_quartile.csv")
+    used_vars = used_vars["variables"]
+    hh_columns, b_columns = columns_in_vars(used_vars)
+
+    hh_filter_columns = ["building_id", "large_area_id", "mcd_model_quota", "year_built", "residential_units"]
+    b_filter_columns = ["large_area_id", "mcd_model_quota"]
+
+    hh_var = hh_columns + hh_filter_columns
+    b_var = b_columns + b_filter_columns
+    hh, b = load_hlcm_df(hh_var, b_var)
     # sampling hh
+    # from the new move-ins, last 5-10 years
+    # weighted by mcd_quota
     hh = hh[hh.large_area_id == 125]
     hh = hh[hh.building_id > 1]
-    # hh = hh.sample(estimation_sample_size)
-    hh = hh.sample(hh_sample_size)
+    hh = hh[hh.residential_units > 0]
+    hh = hh[hh.year_built > 2010]
+    hh["mcd_model_quota"] += 1 # add 1 to all hh's mcd_model_quota for weights
+    hh = hh.sample(hh_sample_size, weights="mcd_model_quota")
     picked_bid = hh["building_id"]
     hh = hh.reset_index()
     hh = hh.fillna(0)
     # sampling b
+    # building age < 10
+    # weighted by mcd_quota
     b = b[b.large_area_id == 125]
     b = b[b.residential_units > 0]
-    b_sample = b[~b.index.isin(hh.building_id)].sample((estimation_sample_size-1)*hh_sample_size)
+    b["mcd_model_quota"] += 1 # add 1 to all buildings's mcd_model_quota for weights
+    b_sample = b[~b.index.isin(hh.building_id)].sample( # replace sample or not
+        (estimation_sample_size-1)*hh_sample_size, replace=False, weights="mcd_model_quota")
     b_sample = pd.concat([b.loc[hh.building_id], b_sample])
     b_sample = b_sample.reset_index()
     b_sample = b_sample.fillna(0)
     # remove unnecessary col in HH
-    hh = hh[[col for col in hh.columns if 'id' not in col ]]
+    hh = hh[[col for col in hh.columns if col not in hh_filter_columns+["household_id"]]]
     # remove unnecessary col in buildings
-    b_sample = b_sample[[col for col in b_sample.columns if col not in [ 'large_area_id']]]
+    b_sample = b_sample[[col for col in b_sample.columns if col not in b_filter_columns]]
 
-    X_df = pd.concat([pd.concat([hh]*estimation_sample_size).reset_index(), b_sample], axis=1)
+    X_df = pd.concat([pd.concat([hh]*estimation_sample_size).reset_index(drop=True), b_sample], axis=1)
     # Y: 1 for the building picked
     Y = X_df.building_id.isin(picked_bid).astype(int).values
-    X_df = X_df[[col for col in X_df.columns if col not in ['building_id', 'index']]]
+    # remove extra cols
+    X_df = X_df[[col for col in X_df.columns if col not in ['building_id']]]
     orig_var_names = X_df.columns
+    # create interaction variables
+    newX_cols_name = used_vars.values 
+    X_wiv = np.array([])
+    for varname in newX_cols_name:
+        if X_wiv.size > 0:
+            X_wiv = np.concatenate((X_wiv, get_interaction_vars(X_df, varname)), axis=1)
+        else:
+            X_wiv = get_interaction_vars(X_df, varname)
+            
     # print("orig_var_names",orig_var_names)
     # df to ndarray
-    X = X_df.values
-    # standardize X
+    X = X_wiv
+    # only keep variables with variation
     used_val = np.arange(X.shape[1])[np.std(X, axis=0, dtype=np.float64) > 0]
+    # standardize X
     X = X[:, np.std(X, axis=0, dtype=np.float64) > 0]
     X = (X - np.mean(X, axis=0)) / np.std(X, axis=0, dtype=np.float64)
     # shuffle X
@@ -194,26 +266,36 @@ if RELOAD:
     theta = np.zeros((X.shape[1], 1))
     # cache input tables
     cache_hdf = pd.HDFStore('lcm_testing.hdf')
-    cache_hdf["theta"] = theta
-    cache_hdf["X"] =X 
-    cache_hdf["Y"] =Y 
-    cache_hdf["Y_onehot"] =Y_onehot 
-    cache_hdf["available_choice"] =available_choice 
+    cache_hdf["theta"] = pd.DataFrame(theta)
+    cache_hdf["X"] = pd.DataFrame(X)
+    cache_hdf["Y"] = pd.DataFrame(Y)
+    cache_hdf["Y_onehot"] = pd.DataFrame(Y_onehot) 
+    cache_hdf["available_choice"] = pd.DataFrame(available_choice)
     cache_hdf.close()
 else:
     cache_hdf = pd.HDFStore('lcm_testing.hdf', 'r')
     theta, X, Y, Y_onehot, available_choice = cache_hdf['theta'], cache_hdf['X'], cache_hdf['Y'], cache_hdf['Y_onehot'], cache_hdf['available_choice']
     theta, X, Y, Y_onehot, available_choice = theta.values, X.values, Y.values, Y_onehot.values, available_choice.values
 
+# dtypes conversion
 X = {0:X, 1:X}
 theta = {0:theta, 1:theta}
-Y = 1 - Y
+Y = 1 - Y # 0 means picked, 1 means not picked
 Y_onehot = np.concatenate((Y_onehot, 1-Y_onehot), axis=1)
 available_choice = np.concatenate((available_choice, available_choice), axis=1)
+
+t0 = time.time()
 theta_optim_full = minimize(theta, neglog_DCM, -20000, X, Y, Y_onehot, available_choice)
+t1 = time.time()
 # print([orig_var_names[used_val][i] for i in theta_optim_full.argsort()[0][:20]])
 # print(Y.argsort()[::-1][:20])
 # print(X.dot(theta_optim_full[0]).reshape(-1).argsort()[::-1][:20])
 # print([x for x in Y.reshape(-1).argsort()[::-1][:hh_sample_size] if x in X.dot(theta_optim_full[0]).reshape(-1).argsort()[::-1][:hh_sample_size]])
+
+# exporting theta
+out_theta = pd.DataFrame(theta_optim_full[0], columns=['theta'])
+out_theta.index = newX_cols_name[used_val]
+out_theta = out_theta.sort_values(by='theta', ascending=False)
+out_theta.to_csv('out_theta.txt')
 
 print('done')
