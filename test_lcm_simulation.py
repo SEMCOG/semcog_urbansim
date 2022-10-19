d1e36d80a08cac705a9e71074195b2427f29e7ed
Tian Xie <xie@semcog.org>
2022-10-19 17:59:34 -0400

    Updated estimation with urbansim modelmanager; removed extra code in estimation

---
+++ test_lcm_simulation.py
@@ -0,0 +1,24 @@
+import orca
+import os
+import yaml
+from urbansim_templates.models import LargeMultinomialLogitStep
+from urbansim_templates import modelmanager as mm
+mm.initialize('configs/elcm_2050')
+
+
+def generate_yaml_configs():
+    hlcm_yaml = os.listdir('configs/hlcm_2050')
+    hlcm_yaml = ["hlcm_2050/"+path for path in hlcm_yaml if '.yaml' in path]
+
+    elcm_yaml = os.listdir('configs/elcm_2050')
+    elcm_yaml = ["elcm_2050/"+path for path in elcm_yaml if '.yaml' in path]
+    obj = {
+        'hlcm': hlcm_yaml,
+        'elcm': elcm_yaml
+    }
+    with open("./configs/yaml_configs_2050.yaml", 'w') as f:
+        yaml.dump(obj, f, default_flow_style=False)
+
+if __name__ == "__main__":
+    import models
+    print('done')
