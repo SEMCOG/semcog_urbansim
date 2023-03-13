c7c4a0bb6b51e57bbdebdbb67aa8d53c7e5176c8
Tian Xie <xie@semcog.org>
2023-03-13 16:09:10 -0400

    Updated output_indicators_run_test.py

--- output_indicators_run_test.py
+++ output_indicators_run_test.py
@@ -8,5 +8,20 @@ import utils
 from urbansim.utils import misc, networks
 import output_indicators
 
-data_out = './runs/run339.h5'
-output_indicators.main(data_out)
+# set up run
+base_year = 2020
+final_year = 2050
+indicator_spacing = 5
+upload_to_carto = True
+run_debug = False
+add_2019 = True
+data_out = './runs/run1161.h5'
+
+output_indicators.main(
+    data_out,
+    base_year,
+    final_year,
+    spacing=indicator_spacing,
+    upload_to_carto=upload_to_carto,
+    add_2019=add_2019,
+)
