0cb5479dce4757bebea7100bcac5200136bfdf36
Tian Xie <xie@semcog.org>
2023-09-21 16:26:29 -0400

    Fixed parcels joining steps where parcels may intersect multiple districts or intersect no district

--- prep/parcel_to_legislative_district.py
+++ prep/parcel_to_legislative_district.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import geopandas as gpd
+import tobler
 from sqlalchemy import create_engine, text
 
 # set connection string, in terminal
@@ -26,9 +27,30 @@ if __name__ == "__main__":
     mi_senate = mi_senate[['mi_senate_id', 'geometry']]
 
     # spatial join
-    print('joining')
-    parcels = parcels.sjoin(mi_house, how='left')[['geom', 'parcel_id', 'mi_house_id']]
-    parcels = parcels.sjoin(mi_senate, how='left')[['geom', 'parcel_id', 'mi_house_id', 'mi_senate_id']]
+    print('joining michigan house')
+    parcels = parcels.sjoin(mi_house, how='left', predicate='within')[['geom', 'parcel_id', 'mi_house_id']]
+    # fix parcel intersect 2+ districts
+    failed_parcels = parcels.loc[parcels['mi_house_id'].isna(), ['geom', 'parcel_id']]
+    parcels.loc[failed_parcels.index, 'mi_house_id'] = tobler.area_weighted.area_join(mi_house, failed_parcels, ['mi_house_id'])['mi_house_id']
+
+    # fix parcel not intersecting any district
+    failed_parcels = parcels.loc[parcels['mi_house_id'].isna(), ['geom', 'parcel_id']]
+    parcels.loc[failed_parcels.index, 'mi_house_id'] = failed_parcels.sjoin_nearest(mi_house, how='left')['mi_house_id']
+
+    print('joining michigan senate')
+    parcels = parcels.sjoin(mi_senate, how='left', predicate='within')[['geom', 'parcel_id', 'mi_house_id', 'mi_senate_id']]
+
+    # fix parcel intersect 2+ districts
+    failed_parcels = parcels.loc[parcels['mi_senate_id'].isna(), ['geom', 'parcel_id']]
+    parcels.loc[failed_parcels.index, 'mi_senate_id'] = tobler.area_weighted.area_join(mi_senate, failed_parcels, ['mi_senate_id'])['mi_senate_id']
+
+    # fix parcel not intersecting any district
+    failed_parcels = parcels.loc[parcels['mi_senate_id'].isna(), ['geom', 'parcel_id']]
+    parcels.loc[failed_parcels.index, 'mi_senate_id'] = failed_parcels.sjoin_nearest(mi_senate, how='left')['mi_senate_id']
+
+    # quality controls
+    assert parcels.isna().sum().sum() == 0
+    assert parcels.parcel_id.duplicated().sum() == 0
 
     # save parcel_id to ids
     parcels[['parcel_id', 'mi_house_id', 'mi_senate_id']].to_csv('data/parcel_to_legis.csv', index=False)
