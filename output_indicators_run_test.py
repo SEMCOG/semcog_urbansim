0a9d79345c716f482fd59c57bd28f1fe80d808b2
Tian Xie <xie@semcog.org>
2023-12-06 15:58:31 -0500

    Updated output_indicator run script

--- output_indicators_run_test.py
+++ output_indicators_run_test.py
@@ -3,6 +3,9 @@ import shutil
 
 import os
 orca.add_injectable('use_checkpoint', False)
+# hlcm configs
+orca.add_injectable('hlcm_model_path', '/mnt/hgfs/RDF2050/estimation/models/models_23nov30')
+orca.add_injectable('yaml_configs', 'yaml_configs_nn.yaml')
 
 import models
 import utils
@@ -16,7 +19,8 @@ indicator_spacing = 5
 upload_to_carto = True
 run_debug = False
 add_2019 = True
-data_out = './runs/run2120_TAZ_draft_final.h5'
+# data_out = './runs/run2120_TAZ_refinement_090823_school_legis.h5'
+data_out = 'runs/run1241.h5'
 
 output_indicators.main(
     data_out,
