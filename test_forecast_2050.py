425110f51ec3b17e82245183f59e61aa701c7f94
Tian Xie <xie@semcog.org>
2024-02-20 10:56:17 -0500

    Fixed caching hh_seed steps and update simulation code

--- test_forecast_2050.py
+++ test_forecast_2050.py
@@ -20,7 +20,7 @@ run_debug = False
 add_2019 = True
 
 # hlcm configs
-orca.add_injectable('hlcm_model_path', '/mnt/hgfs/RDF2050/estimation/models/models_24jan16')
+orca.add_injectable('hlcm_model_path', '/mnt/hgfs/RDF2050/estimation/models/models_24Feb15')
 orca.add_injectable('yaml_configs', 'yaml_configs_nn.yaml')
 
 orca.add_injectable('base_year', base_year)
