3e3e0642040369d42dfbe803742cfa61b8d419b1
Tian Xie <xie@semcog.org>
2024-06-03 15:55:15 -0400

    #51 Implemented artificially bump up dearborn hh_pop trend variables

--- models.py
+++ models.py
@@ -379,6 +379,20 @@ def update_taz_hlcm_trend(taz_hlcm_trend_by_year, year, households, buildings):
         prev_df = taz_hlcm_trend_by_year[str(cur_year)]
         cur_df = taz_hlcm_trend_by_year[str(cur_year)]
     diff = cur_df - prev_df
+
+    # Experimental: 
+    # * For Dearborn, taz zone 420-472,
+    selected_taz_ids = [idx for idx in range(420, 473) if idx in diff.index]
+    N = len(selected_taz_ids) # total number of applicable TAZs
+    # increase hh_count by 50%, (distributed evenly among TAZs, same method below)
+    diff.loc[selected_taz_ids, 'hh_count'] += (max(diff.loc[selected_taz_ids, 'hh_count'].sum() // 2, 1000 ) // (N)) 
+    # increase hh_pop by 100%pp
+    diff.loc[selected_taz_ids, 'hh_pop'] += (max(diff.loc[selected_taz_ids, 'hh_pop'].sum(), 2000 ) // (N)) 
+    # increase with_children hh by 100%
+    diff.loc[selected_taz_ids, 'with_children'] += (max(diff.loc[selected_taz_ids, 'with_children'].sum(), 2000 ) // (N)) 
+    # reduce one_persons_hh count by 50%
+    diff.loc[selected_taz_ids, 'one_person_hh'] -= (max(diff.loc[selected_taz_ids, 'one_person_hh'].sum() // 2, 1000 ) // (N)) 
+
     for var in df_cur.columns:
         print("registering building variable", var+"_taz_5yr_change")
         @orca.column("buildings", var+"_taz_5yr_change")
