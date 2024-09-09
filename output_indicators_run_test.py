0dc159b6984c22cc1a1b39a8bf78bab7ee544c31
Tian Xie <xie@semcog.org>
2024-09-09 15:18:56 -0400

    Added handler for missing elcm scenario controls

--- output_indicators_run_test.py
+++ output_indicators_run_test.py
@@ -10,9 +10,11 @@ orca.add_injectable('yaml_configs', 'yaml_configs_nn.yaml')
 # scenario controls
 orca.add_injectable('ENABLE_SCENARIO', True)
 orca.add_injectable('scenario_hh_control_path',
-    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/low_immigration/annual_household_control_totals_2050_06262024.csv')
+    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/low_immigration/annual_household_control_totals_2050_07232024.csv')
 orca.add_injectable('scenario_remi_total_pop',
-    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/low_immigration/remi_total_pop_la06262024.csv')
+    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/low_immigration/remi_total_pop_la07232024.csv')
+orca.add_injectable('scenario_emp_control_path',
+    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/low_immigration/annual_employment_control_totals.csv')
 
 import models
 import utils
