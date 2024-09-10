8a9490064441a33a370adc72c9e1f778875d020d
Tian Xie <xie@semcog.org>
2024-09-10 12:02:01 -0400

    run1283 adding elcm nn run

--- test_forecast_2050.py
+++ test_forecast_2050.py
@@ -32,9 +32,9 @@ orca.add_injectable('final_year', final_year)
 # scenario controls
 orca.add_injectable('ENABLE_SCENARIO', False)
 orca.add_injectable('scenario_hh_control_path',
-    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/new_baseline_outway_big_hhs/annual_household_control_totals_2050_08102024.csv')
+    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/new_baseline/annual_household_control_totals_2050_07012024.csv')
 orca.add_injectable('scenario_remi_total_pop',
-    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/new_baseline_outway_big_hhs/remi_total_pop_la08102024.csv')
+    '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/new_baseline/remi_total_pop_la07012024.csv')
 # orca.add_injectable('scenario_emp_control_path',
 #     '/mnt/hgfs/urbansim/RDF2050/scenarios/controls/high_proftech/annual_employment_control_totals.csv')
 
